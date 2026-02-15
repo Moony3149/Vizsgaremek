f6891e223dad7f35b44a70af304d22ca5fcde252
Moony3149 <zimrakatz31@gmail.com>
2026-02-15 19:14:48 +0100

    Delete Vizsgaremek directory
    Nem aktuális

--- Vizsgaremek/fix_admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "connect.php";
-$pass = password_hash('admin123', PASSWORD_DEFAULT);
-$conn->query("INSERT INTO users (name, userName, email, password, admin) 
-              VALUES ('Admin', 'admin', 'admin@bolt.hu', '$pass', 'user')");
-echo "Admin létrehozva! Email: admin@bolt.hu | Jelszó: admin123";
-?>
